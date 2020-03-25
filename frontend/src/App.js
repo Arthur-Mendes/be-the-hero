4e0606642aa16d635deccad4e73e95aa93c66c00
Arthur-Mendes <arthurmendes22@gmail.com>
2020-03-25 02:38:01 -0300

    back-end da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Fiquem em casa... Aprendam coisas novas... E só uma fase!</p>
+        <small>Arthur Mendes...</small>
+      </header>
+    </div>
+  );
+}
+
+export default App;
